b81c08efe2e79641725560516b47aea99a8a26ca
Amir Yahya Alagar <z.sacramir@gmail.com>
2020-09-23 17:42:12 +0800

    updated some designs

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg mynav">
   <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
+  <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link font-color" href="{{ route('show.home') }}">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link font-color" href="{{ route('show.home') }}">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link font-color" href="{{ route('show.projects') }}">My Projects</a>
@@ -19,3 +19,14 @@
     </ul>
   </div>
 </nav>
+
+<script>
+  const currentLocation = location.href;
+  const menuItem = document.querySelectorAll('a');
+  const menuLength = menuItem.length
+  for (let i = 0; i<menuLength; i++){
+    if(menuItem[i].href === currentLocation){
+      menuItem[i].className = "active nav-link font-color"
+    }
+  }
+</script>
